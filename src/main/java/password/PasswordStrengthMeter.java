4366e3e8d3133d0aa395fc2c40dad668e8e770fb
kihwankim <clack2933@gmail.com>
2021-08-02 19:09:19 +0900

    refactor: 가독성을 위한 코드 추출

--- src/main/java/password/PasswordStrengthMeter.java
+++ src/main/java/password/PasswordStrengthMeter.java
@@ -6,15 +6,21 @@ public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
 
-        int metCounts = 0;
-        if (s.length() >= MIN_LEN_OF_PASSWORD) metCounts++;
-        if (isContainsNum(s)) metCounts++;
-        if (isContainingUppercase(s)) metCounts++;
+        int metCounts = getMetCounts(s);
 
         if (metCounts <= 1) return PasswordStrength.WEAK;
         if (metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
+    }
+
+    private int getMetCounts(String s) {
+        int metCounts = 0;
+        if (s.length() >= MIN_LEN_OF_PASSWORD) metCounts++;
+        if (isContainsNum(s)) metCounts++;
+        if (isContainingUppercase(s)) metCounts++;
+
+        return metCounts;
     }
 
     private boolean isContainsNum(String s) {
