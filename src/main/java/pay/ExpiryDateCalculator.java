e5bff5ee7e92dc4fe6435c2bd3134192a4b3789c
kihwankim <clack2933@gmail.com>
2021-09-02 20:36:36 +0900

    refactor: 첫 납부일지가 정의되어있을 때 코드 리팩토링

--- src/main/java/pay/ExpiryDateCalculator.java
+++ src/main/java/pay/ExpiryDateCalculator.java
@@ -14,21 +14,28 @@ public class ExpiryDateCalculator {
             throw new IllegalArgumentException();
         }
 
-        int usingMonth = payData.getPayAmount() / ONE_MONTH_FEE;
+        int addedMonths = payData.getPayAmount() / ONE_MONTH_FEE;
 
         if (payData.getFirstBillingDate() != null) {
-
-            LocalDate canditateExp = payData.getBillingDate().plusMonths(usingMonth);
-
-            if (payData.getFirstBillingDate().getDayOfMonth() != canditateExp.getDayOfMonth()) {
-                if (YearMonth.from(canditateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
-                    return canditateExp.withDayOfMonth(YearMonth.from(canditateExp).lengthOfMonth());
-                }
-
-                return canditateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
-            }
+            return expireDateUsingFirstBillingDate(payData, addedMonths);
         }
 
-        return payData.getBillingDate().plusMonths(usingMonth);
+        return payData.getBillingDate().plusMonths(addedMonths); // 첫 달이 언제인지 모르는 경우
+    }
+
+    private LocalDate expireDateUsingFirstBillingDate(PayData payData, int usingMonth) {
+        // 31에 처음 납부 현재 2월 인 경우
+        LocalDate canditateExp = payData.getBillingDate().plusMonths(usingMonth);
+
+        if (payData.getFirstBillingDate().getDayOfMonth() != canditateExp.getDayOfMonth()) { // 달의 날짜 길이 비교
+            int dayLenOfCandiMon = YearMonth.from(canditateExp).lengthOfMonth();
+            if (dayLenOfCandiMon < payData.getFirstBillingDate().getDayOfMonth()) {
+                return canditateExp.withDayOfMonth(dayLenOfCandiMon); // 짧은 경우
+            }
+
+            return canditateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth()); // 긴경우
+        }
+
+        return canditateExp; // 일반 경우
     }
 }
